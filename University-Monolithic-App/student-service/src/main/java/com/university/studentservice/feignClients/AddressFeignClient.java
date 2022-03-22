aedf2baa2288a9b8b9731bf388f045b023b93b3c
DeyvidAdzhemiev <60064484+DeyvidAdzhemiev@users.noreply.github.com>
2022-03-22 15:15:52 +0200

    Implement Feign client

--- University-Monolithic-App/student-service/src/main/java/com/university/studentservice/feignClients/AddressFeignClient.java
+++ University-Monolithic-App/student-service/src/main/java/com/university/studentservice/feignClients/AddressFeignClient.java
@@ -1,5 +1,13 @@
 package com.university.studentservice.feignClients;
 
+import com.university.studentservice.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "${address.service.url}", value = "address-feign-client")
 public interface AddressFeignClient {
+    @GetMapping("getById/{id}")
+    public AddressResponse getById(@PathVariable long id);
 
 }
